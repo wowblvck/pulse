a728f36ffdd44d5660237555dd3ae5c8d9bf79f0
Indar Basto <indar.basto@gmail.com>
2022-05-16 19:20:09 +0300

    feat: add js/script.js

---
+++ src/js/script.js
@@ -0,0 +1,48 @@
+// let name = "Ivan";
+// let number = 4;
+
+// let leftBorderWidth = 200;
+
+// let obj = {
+//     name: "apple",
+//     color: "red",
+//     weight: "200"
+// }
+
+// let answer = prompt("Вам есть 18?", "");
+// console.log(answer);
+
+// let isChecked = false,
+//     isClose = false;
+
+// // console.log(isChecked && isClose);
+
+// console.log(isChecked || isClose);
+
+
+// let answer = confirm("Вам есть 18?", "");
+// if (answer) {
+//     console.log("Проходите")
+// } else {
+//     console.log("Уходи")
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log("Неправильно")
+// } else if (num > 100) {
+//     console.log("Много")
+// } else {
+//     console.log("Верно")
+// }
+
+// for (let i = 1; i < 8; i++) {
+//     console.log(i)
+// }
+
+function logging(a, b) {
+    console.log(a + b)
+}
+
+logging(5, 8);
